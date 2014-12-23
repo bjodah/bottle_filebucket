df39e0a78c121dec98df06be4edac74701daba04
Björn Dahlgren <bjodah@gmail.com>
2014-12-23 20:37:31 +0100

    Add hooks.

--- bottle_filebucket.py
+++ bottle_filebucket.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 """
-Server which lets you upload files securely over https.
+Simple Bottle server which lets you upload files securely over https.
 
-On your server (after having installed cherrypy and edited conf.json):
+On your server (after having installed cherrypy and edited config.json):
 
     $ nohup python3 bottle_filebucket.py &
 
@@ -19,56 +19,65 @@ http://dgtool.blogspot.se/2011/12/ssl-encryption-in-python-bottle.html
 import json
 import os
 import subprocess
+
+import argh
 from bottle import route, run, request, Bottle, server_names, ServerAdapter
 
-config = json.load(open('config.json'))
-secret_token = config['secret_token']
-cert_path = config['certificate_path']
-priv_key_path = config['private_key_path']
-destdir = config['destdir']
-host = config['host']
-port = config['port']
+def main(config_path='config.json', generate_cert_if_missing=False):
 
+    config = json.load(open(config_path))
+    secret_token = config['secret_token']
+    cert_path = config['certificate_path']
+    priv_key_path = config['private_key_path']
+    destdir = config['destdir']
+    host = config['host']
+    port = config['port']
+    hooks = config['hooks']
+    upload_route = config['upload_route']
 
-class MySSLCherryPy(ServerAdapter):
-    def run(self, handler):
-        from cherrypy import wsgiserver
-        from cherrypy.wsgiserver.ssl_builtin import BuiltinSSLAdapter
-        server = wsgiserver.CherryPyWSGIServer((self.host, self.port), handler)
-        server.ssl_adapter = BuiltinSSLAdapter(cert_path, priv_key_path)
-        try:
-            server.start()
-        finally:
-            server.stop()
+    class MySSLCherryPy(ServerAdapter):
+        def run(self, handler):
+            from cherrypy import wsgiserver
+            from cherrypy.wsgiserver.ssl_builtin import BuiltinSSLAdapter
+            server = wsgiserver.CherryPyWSGIServer((self.host, self.port), handler)
+            server.ssl_adapter = BuiltinSSLAdapter(cert_path, priv_key_path)
+            try:
+                server.start()
+            finally:
+                server.stop()
 
-server_names['mysslcherrypy'] = MySSLCherryPy
+    server_names['mysslcherrypy'] = MySSLCherryPy
 
-app = Bottle()
+    app = Bottle()
 
+    @app.route(upload_route, method='PUT')
+    def upload():
+        given_token = request.forms.get("token")
+        if given_token != secret_token:
+            return {'success': False, 'error': "Invalid token."}
+        fileobj = request.files['filedata']
 
-@app.route("/upload", method='PUT')
-def upload():
-    given_token = request.forms.get("token")
-    if given_token != secret_token:
-        return {'success': False, 'error': "Invalid token."}
-    fileobj = request.files['filedata']
+        if fileobj:
+            filename = request.forms.get("filename", fileobj.filename)
+            for char in r"^<>/{}[]\()~`*$":
+                # naive filename sanitizer
+                if char in fileobj.filename:
+                    return {'success': False,
+                            'error': "Invalid characters in filename"}
+            open(os.path.join(destdir, filename), 'wb').write(fileobj.file.read())
+        for hook in hooks:
+            subprocess.call(hook % {'filename': filename}, shell=True)
+        return {'success': True}
 
-    if fileobj:
-        filename = request.forms.get("filename", fileobj.filename)
-        for char in r"^[^<>/{}[\]~`]*$":
-            if char in fileobj.filename:
-                return {"success": False,
-                        "error": "Invalid characters in filename"}
-        open(os.path.join(destdir, filename), 'wb').write(fileobj.file.read())
-
-    return {"success": True}
-
-
-if __name__ == '__main__':
-    if not os.path.exists(cert_path) and\
+    if generate_cert_if_missing and\
+       not os.path.exists(cert_path) and\
        not os.path.exists(priv_key_path):
         subprocess.check_call(
             'openssl req -new -x509 -keyout %s -out %s -days 3650 -nodes -subj'
             ' "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com"' % (
                 priv_key_path, cert_path), shell=True)
     run(app, host=host, port=port, debug=True, server='mysslcherrypy')
+
+
+if __name__ == '__main__':
+    argh.dispatch_command(main)
--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#openssl req -new -x509 -keyout bottle_filebucket.pem -out bottle_filebucket.crt -days 3650 -nodes 
-python3 bottle_filebucket.py &
+nohup python3 bottle_filebucket.py --generate-cert-if-missing &
 PID=$!
 sleep 3 # give the server a chance to start
 mkdir temp/
@@ -8,6 +7,9 @@ oricwd=$(pwd)
 function finish {
     cd $oricwd
     rm -r temp/
+    rm file2.txt
+    rm file2.txt.tail1
+    rm nohup.out
     kill $PID
 }
 trap finish EXIT
@@ -15,8 +17,14 @@ cd temp/
 echo A >file1.txt
 curl -i -X PUT -F token=BLS4G66XHBGOI -F filename=file2.txt -F filedata=@file1.txt --insecure "https://localhost:8081/upload"
 if [[ `cat ../file2.txt` != `cat file1.txt` ]]; then
-    echo "Test FAILED!"
+    echo "Test FAILED! (files mismatch)"
     exit 1
 fi
-echo "Test PASSED!"
+if [[ `cat ../file2.txt.tail1` != `tail -n 1 file1.txt` ]]; then
+    echo "Test FAILED! (second hook failed)"
+    exit 1
+fi
+green='\033[0;32m'
+NC='\033[0m' # No Color
+echo -e "\nTest ${green}PASSED!${NC}"
 exit 0
